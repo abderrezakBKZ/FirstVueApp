1be25c7a84ec2a8b7e36ff5b759fec43d07d18a6
abderrezak bkz <abderrezak.bkz@gmail.com>
2020-12-28 17:03:03 +0100

    70% of the work

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+  <b-jumbotron >
+    <template #header></template>
+
+    <template #lead>
+      {{currentQuestion.question}}
+    </template>
+
+    <hr class="my-4">
+
+    <p v-for="(answer,index) in answers" :key="index">
+     {{answer}}
+    </p>
+
+    <b-button variant="primary" href="#">Submit</b-button>
+    <b-button @click="next" variant="success" href="#">Next</b-button>
+  </b-jumbotron>
+</div>
+</template>
+
+<script>
+export default {
+    props : {
+        currentQuestion : Object,
+        next : Function
+    },
+    computed: {
+    answers () {
+       let answers = [...this.currentQuestion.incorrect_answers]
+        answers.push (this.currentQuestion.correct_answer)
+        return answers
+       
+     }
+}
+}
+
+
+
+</script>
---
+++ src/components/tete.vue
@@ -0,0 +1,8 @@
+<template>
+<div>
+  <b-nav tabs>
+    <b-nav-item disabled> <b>Quiz in english</b> </b-nav-item>
+    <b-nav-item disabled>counter 6/10</b-nav-item>
+  </b-nav>
+</div>
+</template>
